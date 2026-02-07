88af81589b7e661aefec015d27f6d4a5065ba597
AhmedkamalR <you@example.comahmedkamalrakha8@gmail.com>
2026-02-07 13:35:05 +0200

    Integrate Faker for dynamic test data generation

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -1,14 +1,15 @@
 import { test, expect } from '@playwright/test';
+import {faker} from '@faker-js/faker';
 
 test('should be able to add a todo', async ({ page }) => {
   // Generate a unique email to avoid "already registered" errors
-  const uniqueEmail = `testuser${Date.now()}@test.com`;
+  // const uniqueEmail = `testuser${Date.now()}@test.com`;
 
   await page.goto('/signup');
 
-  await page.fill('[data-testid="first-name"]', 'ahmed');
-  await page.fill('[data-testid="last-name"]', 'kamal');
-  await page.fill('[data-testid="email"]', uniqueEmail);
+  await page.fill('[data-testid="first-name"]', faker.person.firstName());
+  await page.fill('[data-testid="last-name"]', faker.person.lastName());
+  await page.fill('[data-testid="email"]', faker.internet.email());
   await page.fill('[data-testid="password"]', 'Test@1234');
   await page.fill('[data-testid="confirm-password"]', 'Test@1234');
   await page.click('[data-testid="submit"]');
@@ -26,13 +27,13 @@ test('should be able to add a todo', async ({ page }) => {
 
 test('should be able to delete a todo', async ({ page }) => {
   // Generate a unique email to avoid "already registered" errors
-  const uniqueEmail = `testuser${Date.now()}@test.com`;
+  // const uniqueEmail = `testuser${Date.now()}@test.com`;
 
   await page.goto('/signup');
 
-  await page.fill('[data-testid="first-name"]', 'ahmed');
-  await page.fill('[data-testid="last-name"]', 'kamal');
-  await page.fill('[data-testid="email"]', uniqueEmail);
+  await page.fill('[data-testid="first-name"]', faker.person.firstName());
+  await page.fill('[data-testid="last-name"]', faker.person.lastName());
+  await page.fill('[data-testid="email"]', faker.internet.email());
   await page.fill('[data-testid="password"]', 'Test@1234');
   await page.fill('[data-testid="confirm-password"]', 'Test@1234');
   await page.click('[data-testid="submit"]');
