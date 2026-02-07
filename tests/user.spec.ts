d22ce1cba0928942d252a01329b5d21e98751170
AhmedkamalR <you@example.comahmedkamalrakha8@gmail.com>
2026-02-07 18:28:49 +0200

    Extend Page Object Model with TodoPage and improve test readability

--- tests/user.spec.ts
+++ tests/user.spec.ts
@@ -2,16 +2,16 @@ import { test, expect } from '@playwright/test';
 import { faker } from '@faker-js/faker';
 import User from '../models/User';
 import RegisterPage from '../pages/RegisterPage';
+import TodoPage from '../pages/TodoPage';
 
 test('should be able to register to the todo website', async ({ page }) => {
   const user = new User(faker.person.firstName(), faker.person.lastName(), faker.internet.email(), 'Test@1234');
 
   const registerPage = new RegisterPage(page);
-
   await registerPage.load();
   await registerPage.register(user);
-
-  const welcomeMessage = page.locator('[data-testid="welcome"]');
-
+  const todoPage = new TodoPage(page);
+  
+  const welcomeMessage = todoPage.getWelcomeMessage();
   await expect(welcomeMessage).toBeVisible();
 });
